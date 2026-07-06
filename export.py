f8a7960865677a0c1aa1808038d9979c21746b59
Jimena Arruti <jimearruti@gmail.com>
2026-07-06 14:13:43 +0000

    remove realtime option

--- export.py
+++ export.py
@@ -13,7 +13,6 @@ class args(Config):
     RUN = None
     DATA = False
     OUT_DIR = "export"
-    REALTIME = False
 
 
 args.parse_args()
@@ -22,23 +21,17 @@ export_dir = path.join(args.OUT_DIR, run_string)
 makedirs(export_dir, exist_ok=True)
 
 class ScriptDDSP(torch.nn.Module):
-    def __init__(self, ddsp, mean_loudness, std_loudness, realtime):
+    def __init__(self, ddsp, mean_loudness, std_loudness):
         super().__init__()
         self.ddsp = ddsp
         self.ddsp.gru.flatten_parameters()
 
         self.register_buffer("mean_loudness", torch.tensor(mean_loudness))
         self.register_buffer("std_loudness", torch.tensor(std_loudness))
-        self.realtime = realtime
 
     def forward(self, pitch, loudness):
         loudness = (loudness - self.mean_loudness) / self.std_loudness
-        if self.realtime:
-            pitch = pitch[:, ::self.ddsp.block_size]
-            loudness = loudness[:, ::self.ddsp.block_size]
-            return self.ddsp.realtime_forward(pitch, loudness)
-        else:
-            return self.ddsp(pitch, loudness)
+        return self.ddsp(pitch, loudness)
 
 
 with open(path.join(args.RUN, "config.yaml"), "r") as config:
@@ -57,8 +50,7 @@ scripted_model = torch.jit.script(
     ScriptDDSP(
         ddsp,
         config["data"]["mean_loudness"],
-        config["data"]["std_loudness"],
-        args.REALTIME,
+        config["data"]["std_loudness"]
     ))
 torch.jit.save(
     scripted_model,
