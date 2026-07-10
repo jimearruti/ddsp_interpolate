13569c89425aac0ef286fc2703be9e60004b7c37
Jimena Arruti <jimearruti@gmail.com>
2026-07-10 07:27:35 +0100

    update generate audio to save results

--- generate_audio.py
+++ generate_audio.py
@@ -41,12 +41,15 @@ instrument_paths = {
     },
 }
 
-
 path_stats_file = "preprocessed/mean_std_loudness.yml"
+results_folder = "results"
+
+if not os.path.exists(results_folder):
+    os.makedirs(results_folder)
+
 stats = yaml.safe_load(open(path_stats_file, "r")) 
 mean_loudness = stats["mean_loudness"]
 std_loudness = stats["std_loudness"]
-
 
 for instrument1, instrument2 in combinations(["vn", "fl", "tpt"], 2):
     print(instrument1, instrument2)
@@ -58,23 +61,24 @@ for instrument1, instrument2 in combinations(["vn", "fl", "tpt"], 2):
     with open(split_files_path, "r") as f:
         validation_files = json.load(f)["val"]
 
-    for model_type in ["fix", "exp", "finetuned"]:
-        for validation_file in validation_files:
-            filename = validation_file.split("/")[-1]
-            x, p, l = preprocess(validation_file, **config["preprocess"])
-            pitch_tensor = torch.from_numpy(p).float().view(1, -1, 1)
-            loudness_tensor = torch.from_numpy(l).float().view(1, -1, 1)
-            loudness_norm = (loudness_tensor - mean_loudness) / std_loudness
+    for validation_file in validation_files:
+        filename = validation_file.split("/")[-1]
+        x, p, l = preprocess(validation_file, **config["preprocess"])
+        pitch_tensor = torch.from_numpy(p).float().view(1, -1, 1)
+        loudness_tensor = torch.from_numpy(l).float().view(1, -1, 1)
+        loudness_norm = (loudness_tensor - mean_loudness) / std_loudness
+        
+        for model_type in ["fix", "exp", "finetuned"]:    
 
             model1 = load_model_from_weights(path1[model_type], config)
             model2 = load_model_from_weights(path2[model_type], config)
 
             # extremes
             instrument1_output = model1.forward(pitch_tensor, loudness_norm).reshape(-1).detach().cpu().numpy()
-            torchaudio.save(f"audios/{filename}_{instrument1}_{model_type}.wav",  instrument1_output, sample_rate=sr)
+            torchaudio.save(f"results/{filename}_{instrument1}_{model_type}.wav",  instrument1_output, sample_rate=sr)
 
             instrument2_output = model2.forward(pitch_tensor, loudness_norm).reshape(-1).detach().cpu().numpy()
-            torchaudio.save(f"audios/{filename}_{instrument2}_{model_type}.wav",  instrument2_output, sample_rate=sr)
+            torchaudio.save(f"results/{filename}_{instrument2}_{model_type}.wav",  instrument2_output, sample_rate=sr)
 
             # interpolated output middle
             alpha = 0.5
@@ -85,7 +89,7 @@ for instrument1, instrument2 in combinations(["vn", "fl", "tpt"], 2):
                 loudness_norm, 
                 alpha=alpha
             )
-            torchaudio.save(f"audios/{filename}_interpolated_output_{instrument1}_{instrument2}_{model_type}_alpha_{alpha}_with_reverb.wav", 
+            torchaudio.save(f"results/{filename}_interpolated_output_{instrument1}_{instrument2}_{model_type}_alpha_{alpha}_with_reverb.wav", 
                             interpolated_output_middle_with_reverb, sample_rate=sr)
 
             interpolated_output_middle_without_reverb = get_interpolated_output(        
@@ -96,20 +100,20 @@ for instrument1, instrument2 in combinations(["vn", "fl", "tpt"], 2):
                 alpha=alpha,
                 reverb=False
             )
-            torchaudio.save(f"audios/{filename}_interpolated_output_{instrument1}_{instrument2}_{model_type}_alpha_{alpha}_without_reverb.wav",  
+            torchaudio.save(f"results/{filename}_interpolated_output_{instrument1}_{instrument2}_{model_type}_alpha_{alpha}_without_reverb.wav",  
                             interpolated_output_middle_without_reverb, sample_rate=sr)
             
             # interpolated output sweep
             n_steps_no_morph=2000
             interpolated_output_sweep_with_reverb = get_interpolated_outputs_sweep(model1, model2, pitch_tensor, loudness_norm, n_steps_no_morph, reverb=True)
-            torchaudio.save(f"audios/{filename}_interpolated_output_sweep_{instrument1}_{instrument2}_{model_type}_with_reverb.wav",  
+            torchaudio.save(f"results/{filename}_interpolated_output_sweep_{instrument1}_{instrument2}_{model_type}_with_reverb.wav",  
                             interpolated_output_sweep_with_reverb, sample_rate=sr)
             interpolated_output_sweep_without_reverb = get_interpolated_outputs_sweep(model1, model2, pitch_tensor, loudness_norm, n_steps_no_morph, reverb=False)
-            torchaudio.save(f"audios/{filename}_interpolated_output_sweep_{instrument1}_{instrument2}_{model_type}_without_reverb.wav",  
+            torchaudio.save(f"results/{filename}_interpolated_output_sweep_{instrument1}_{instrument2}_{model_type}_without_reverb.wav",  
                             interpolated_output_sweep_without_reverb, sample_rate=sr)
 
             # interpolated weights middle
             interpolated_weights_model = get_model_with_interpolated_weights(path1[model_type], path2[model_type], alpha, config)
             interpolated_weighs_audio = interpolated_weights_model(pitch_tensor, loudness_norm)
-            torchaudio.save(f"audios/{filename}_interpolated_weights_{instrument1}_{instrument2}_{model_type}_alpha_{alpha}.wav", 
+            torchaudio.save(f"results/{filename}_interpolated_weights_{instrument1}_{instrument2}_{model_type}_alpha_{alpha}.wav", 
                             interpolated_weighs_audio.reshape(-1).detach().cpu().numpy(), sample_rate=sr)
