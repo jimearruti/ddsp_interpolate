8cfff0906a292ce9e8cf955058793d5dd590ed00
Jimena Arruti <jimearruti@gmail.com>
2026-07-13 10:53:31 +0100

    add normalization per model

--- generate_audio.py
+++ generate_audio.py
@@ -27,20 +27,17 @@ processed_folder = config["preprocess"]["out_dir"]
 
 instrument_paths = {
     "vn": {
-        "fix": "runs/fix_LR/20260629_191900/vn/state.pth",
-        "exp": "runs/exp_LR/20260701_100835/vn/state.pth",
+        "exp": "runs/from_scratch_exp_LR_after_loudness_fix/20260712_130718/vn/state.pth",
         "finetuned": "runs/finetune_exp_LR/20260707_002157/vn/state.pth",
         "exported_model": "export/runs_fix_LR_20260629_191900_vn/ddsp_vn_pretrained.ts",
     },
     "fl": {
-        "fix": "runs/fix_LR/20260629_191900/fl/state.pth",
-        "exp": "runs/from_scratch_exp_LR_after_loudness_fix/20260710_175112/fl/state.pth",     
+        "exp": "runs/from_scratch_exp_LR_after_loudness_fix/20260710_175112/fl/state.pth",      
         "finetuned": "runs/finetune_exp_LR/20260707_002157/fl/state.pth",
         "exported_model": "export/runs_fix_LR_20260629_191900_fl/ddsp_fl_pretrained.ts",
     },
     "tpt": {
-        "fix": "runs/fix_LR/20260629_191900/tpt/state.pth",
-        "exp": "runs/debug/20260706_123700/tpt/state.pth",
+        "exp": "runs/from_scratch_exp_LR_after_loudness_fix/20260712_131725/tpt/state.pth",
         "finetuned": "runs/finetune_exp_LR/20260707_002157/tpt/state.pth",
         "exported_model": "export/runs_fix_LR_20260629_191900_tpt/ddsp_tpt_pretrained.ts",
     },
@@ -61,8 +58,8 @@ if not os.path.exists(path_stats_file):
     get_train_stats_for_dataset(config, batch=16, split_dataset=True)
 
 stats = yaml.safe_load(open(path_stats_file, "r")) 
-mean_loudness = stats["mean_loudness"]
-std_loudness = stats["std_loudness"]
+global_mean_loudness = stats["mean_loudness"]
+global_std_loudness = stats["std_loudness"]
 
 
 for instrument1, instrument2 in combinations(["vn", "fl", "tpt"], 2):
@@ -78,6 +75,21 @@ for instrument1, instrument2 in combinations(["vn", "fl", "tpt"], 2):
     split_files_path = f"{processed_folder}/{instrument1}/split_files.json"
     with open(split_files_path, "r") as f:
         validation_files = json.load(f)["val"]
+
+    instrument1_config_path = "/".join(path1.split(".")[:-1]) + "config.yaml"
+    instrument2_config_path = "/".join(path2.split(".")[:-1]) + "config.yaml"
+
+    with open(instrument1_config_path, "r") as config_file_training:
+        instrument1_config = yaml.safe_load(config_file_training)
+
+    instrument1_mean_loudness = instrument1_config["preprocess"]["mean_loudness"]
+    instrument1_std_loudness = instrument1_config["preprocess"]["std_loudness"]
+
+    with open(instrument2_config_path, "r") as config_file_training:
+        instrument2_config = yaml.safe_load(config_file_training)
+
+    instrument2_mean_loudness = instrument2_config["preprocess"]["mean_loudness"]
+    instrument2_std_loudness = instrument2_config["preprocess"]["std_loudness"]
 
     for validation_file in validation_files:
         filename = validation_file.split("/")[-1]
@@ -96,19 +108,24 @@ for instrument1, instrument2 in combinations(["vn", "fl", "tpt"], 2):
             torch.save(loudness_tensor, loudness_tensor_path)
             torch.save(pitch_tensor, pitch_tensor_path)
 
-        loudness_norm = (loudness_tensor - mean_loudness) / std_loudness
-        
-        for model_type in ["exp"]: #["fix", "exp", "finetuned"]:    
+            loudness_norm_instrument1 = (loudness_tensor - instrument1_mean_loudness) / instrument1_std_loudness
+            loudness_norm_instrument2 = (loudness_tensor - instrument2_mean_loudness) / instrument2_std_loudness
+
+        for model_type in [f"exp"]: #["fix", "exp", "finetuned"]:    
             print(f"the model is {model_type}")
             model1 = load_model_from_weights(path1[model_type], config)
             model2 = load_model_from_weights(path2[model_type], config)
 
             # extremes
-            instrument1_output = model1.forward(pitch_tensor, loudness_norm).reshape(1, -1).detach().cpu()
-            torchaudio.save(f"{results_folder}/{filename}_{instrument1}_{model_type}.wav",  instrument1_output, sample_rate=sr)
+            instrument_1_audio = f"{results_folder}/{filename}_{instrument1}_{model_type}.wav"
+            if not os.path.exists(instrument_1_audio):
+                instrument1_output = model1.forward(pitch_tensor, loudness_norm_instrument1).reshape(1, -1).detach().cpu()
+                torchaudio.save(f"{results_folder}/{filename}_{instrument1}_{model_type}.wav",  instrument1_output, sample_rate=sr)
 
-            instrument2_output = model2.forward(pitch_tensor, loudness_norm).reshape(1, -1).detach().cpu()
-            torchaudio.save(f"{results_folder}/{filename}_{instrument2}_{model_type}.wav",  instrument2_output, sample_rate=sr)
+            instrument_2_audio = f"{results_folder}/{filename}_{instrument2}_{model_type}.wav"
+            if not os.path.exists(instrument_2_audio):
+                instrument2_output = model2.forward(pitch_tensor, loudness_norm_instrument2).reshape(1, -1).detach().cpu()
+                torchaudio.save(f"{results_folder}/{filename}_{instrument2}_{model_type}.wav",  instrument2_output, sample_rate=sr)
 
             # # interpolated output middle
             # alpha = 0.5
